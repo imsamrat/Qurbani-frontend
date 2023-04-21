110b4537c8bce308ff28303a67612fb68ab478bc
imsamrat <imsamrat24@gmail.com>
2023-04-21 15:50:26 +0600

    API Changed

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,1 +1,2 @@
-export const base_url = "https://qurbani-backend.onrender.com/api/";
+export const base_url = "https://qurbani-backend.vercel.app/api/";
+// export const base_url = "https://qurbani-backend.onrender.com/api/";
